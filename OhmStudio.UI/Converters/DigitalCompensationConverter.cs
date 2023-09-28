378d6060f3efb69b5566b84778321222cbafbc4c
MarkWang <zrt2399@163.com>
2023-09-29 05:38:57 +0800

    增加converter

---
+++ OhmStudio.UI/Converters/DigitalCompensationConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace OhmStudio.UI.Converters
+{
+    public class DigitalCompensationConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (double.TryParse(value?.ToString(), out var result))
+            {
+                return result + System.Convert.ToDouble(parameter);
+            }
+            return default(double);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
