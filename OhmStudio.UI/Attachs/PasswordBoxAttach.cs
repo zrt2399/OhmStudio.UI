69020352cc43abff1ec2ccf380c585f629faeb1d
wbx <zrt2399@163.com>
2023-10-18 19:23:05 +0800

    增加PasswordBoxAttach

---
+++ OhmStudio.UI/Attachs/PasswordBoxAttach.cs
@@ -0,0 +1,88 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace OhmStudio.UI.Attachs
+{
+    /// <summary>
+    /// 为PasswordBox控件的Password增加绑定功能。
+    /// </summary>
+    public class PasswordBoxAttach
+    {
+        public static readonly DependencyProperty PasswordProperty =
+            DependencyProperty.RegisterAttached("Password",
+            typeof(string), typeof(PasswordBoxAttach),
+            new FrameworkPropertyMetadata(string.Empty, OnPasswordPropertyChanged));
+
+        public static readonly DependencyProperty IsEnabledProperty =
+            DependencyProperty.RegisterAttached("IsEnabled",
+            typeof(bool), typeof(PasswordBoxAttach), new PropertyMetadata(false, OnIsEnabledChanged));
+
+        private static readonly DependencyProperty IsUpdatingProperty =
+           DependencyProperty.RegisterAttached("IsUpdating", typeof(bool),
+           typeof(PasswordBoxAttach));
+
+        public static void SetIsEnabled(DependencyObject obj, bool value)
+        {
+            obj.SetValue(IsEnabledProperty, value);
+        }
+
+        public static bool GetIsEnabled(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(IsEnabledProperty);
+        }
+
+        public static string GetPassword(DependencyObject obj)
+        {
+            return (string)obj.GetValue(PasswordProperty);
+        }
+
+        public static void SetPassword(DependencyObject obj, string value)
+        {
+            obj.SetValue(PasswordProperty, value);
+        }
+
+        private static bool GetIsUpdating(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(IsUpdatingProperty);
+        }
+
+        private static void SetIsUpdating(DependencyObject obj, bool value)
+        {
+            obj.SetValue(IsUpdatingProperty, value);
+        }
+
+        private static void OnPasswordPropertyChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            PasswordBox passwordBox = sender as PasswordBox;
+            passwordBox.PasswordChanged -= PasswordBox_PasswordChanged;
+            if (!GetIsUpdating(passwordBox))
+            {
+                passwordBox.Password = (string)e.NewValue;
+            }
+            passwordBox.PasswordChanged += PasswordBox_PasswordChanged;
+        }
+
+        private static void OnIsEnabledChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (sender is PasswordBox passwordBox)
+            {
+                if ((bool)e.OldValue)
+                {
+                    passwordBox.PasswordChanged -= PasswordBox_PasswordChanged;
+                }
+                if ((bool)e.NewValue)
+                {
+                    passwordBox.PasswordChanged += PasswordBox_PasswordChanged;
+                }
+            }
+        }
+
+        private static void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            PasswordBox passwordBox = sender as PasswordBox;
+            SetIsUpdating(passwordBox, true);
+            SetPassword(passwordBox, passwordBox.Password);
+            SetIsUpdating(passwordBox, false);
+        }
+    }
+}
