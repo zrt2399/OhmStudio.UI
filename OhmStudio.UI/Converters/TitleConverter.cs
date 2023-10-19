5cc6ffa71b2e723d4619c40885f93b54652f57d8
MarkWang <zrt2399@163.com>
2023-10-19 23:50:20 +0800

    增加converter

---
+++ OhmStudio.UI/Converters/TitleConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using AvalonDock.Controls;
+using AvalonDock.Layout;
+
+namespace OhmStudio.UI.Converters
+{
+    public class TitleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var floatingWindow = value as LayoutDocumentFloatingWindowControl; floatingWindow.Show();
+
+            var mode = floatingWindow.Model as LayoutDocumentFloatingWindow; 
+
+            return mode.SinglePane.SelectedContent.Title;
+
+            //return Application.Current?.MainWindow.Title;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
