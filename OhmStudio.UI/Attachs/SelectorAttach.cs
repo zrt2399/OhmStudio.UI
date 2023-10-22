8dfb3eac2e68a0da6a724af15c5e076c52575025
MarkWang <zrt2399@163.com>
2023-10-22 11:42:48 +0800

    增加SelectorAttach

---
+++ OhmStudio.UI/Attachs/SelectorAttach.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace OhmStudio.UI.Attachs
+{
+    public class SelectorAttach
+    {
+        public static readonly DependencyProperty SelectedItemsProperty =
+            DependencyProperty.RegisterAttached("SelectedItems", typeof(IList), typeof(SelectorAttach));
+
+        public static IList GetSelectedItems(DependencyObject obj)
+        {
+            return (IList)obj.GetValue(SelectedItemsProperty);
+        }
+
+        public static void SetSelectedItems(DependencyObject obj, IList value)
+        {
+            obj.SetValue(SelectedItemsProperty, value);
+        }
+
+        public static readonly DependencyProperty SelectedItemsAttachProperty =
+            DependencyProperty.RegisterAttached("SelectedItemsAttach", typeof(bool), typeof(SelectorAttach), new PropertyMetadata(false, SelectedChangedCallBack));
+
+        public static bool GetSelectedItemsAttach(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(SelectedItemsAttachProperty);
+        }
+
+        public static void SetSelectedItemsAttach(DependencyObject obj, bool value)
+        {
+            obj.SetValue(SelectedItemsAttachProperty, value);
+        }
+
+        private static void SelectedChangedCallBack(DependencyObject obj, DependencyPropertyChangedEventArgs e)
+        {
+            if (obj is Selector selector)
+            {
+                if ((bool)e.NewValue)
+                {
+                    selector.SelectionChanged += Selector_SelectionChanged;
+                }
+                else
+                {
+                    selector.SelectionChanged -= Selector_SelectionChanged;
+                }
+            }
+        }
+
+        private static void Selector_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (sender is Selector selector)
+            {
+                if (selector is DataGrid dataGrid)
+                {
+                    SetSelectedItems(dataGrid, dataGrid.SelectedItems);
+                }
+                else if (selector is ListBox listBox)
+                {
+                    SetSelectedItems(listBox, listBox.SelectedItems);
+                }
+                //BindingExpression bind = selector.GetBindingExpression(SelectedItemsProperty);
+                //bind?.UpdateSource();
+            }
+        }
+    }
+}
