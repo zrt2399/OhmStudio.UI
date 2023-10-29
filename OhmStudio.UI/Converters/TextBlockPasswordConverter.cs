e31b793a5fa9e17b71a46661632d8e5e907a524b
MarkWang <zrt2399@163.com>
2023-10-29 08:54:39 +0800

    样式调整和增加TextBlockPasswordConverter

---
+++ OhmStudio.UI/Converters/TextBlockPasswordConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace OhmStudio.UI.Converters
+{
+    public class TextBlockPasswordConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null)
+            {
+                return new string('●', value.ToString().Length);
+            }
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null)
+            {
+                return new string('●', value.ToString().Length);
+            }
+            return string.Empty;
+        }
+    }
+}
