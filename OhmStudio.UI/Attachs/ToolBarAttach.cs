f61a9646d7727d1be9b34babd2ff80327ff040eb
MarkWang <zrt2399@163.com>
2023-11-15 21:48:11 +0800

    Nuget更新到1.0.7.4，增加ToolBarAttach

---
+++ OhmStudio.UI/Attachs/ToolBarAttach.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace OhmStudio.UI.Attachs
+{
+    public class ToolBarAttach
+    {
+        public static readonly DependencyProperty ToolBarThumbVisibilityProperty =
+            DependencyProperty.RegisterAttached("ToolBarThumbVisibility", typeof(Visibility), typeof(ToolBarAttach), new PropertyMetadata(Visibility.Visible));
+
+        public static Visibility GetToolBarThumbVisibility(DependencyObject obj)
+        {
+            return (Visibility)obj.GetValue(ToolBarThumbVisibilityProperty);
+        }
+
+        public static void SetToolBarThumbVisibility(DependencyObject obj, Visibility value)
+        {
+            obj.SetValue(ToolBarThumbVisibilityProperty, value);
+        }
+    }
+}
