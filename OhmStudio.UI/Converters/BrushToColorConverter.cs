10220c7c286247859b0158f653f17bac7e7dd90b
MarkWang <zrt2399@163.com>
2023-11-26 05:27:27 +0800

    BrushToColorConverter重命名

--- OhmStudio.UI/Converters/BrushToColorConverter.cs
+++ OhmStudio.UI/Converters/BrushToColorConverter.cs
@@ -5,7 +5,7 @@ using System.Windows.Media;
 
 namespace OhmStudio.UI.Converters
 {
-    public class ColorConverter : IValueConverter
+    public class BrushToColorConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
