2a26b7ca76ddac9e8a10c739f7d16edfb4bf0845
TheShy <zrt2399@163.com>
2023-12-13 18:54:41 +0800

    增加2022皮肤运行时的外观颜色资源

---
+++ OhmStudio.UI/PublicMethods/StatusManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace OhmStudio.UI.PublicMethods
+{
+    public static class StatusManager
+    {
+        static Color RunningStatusBarBackground => (Color)Application.Current.Resources["RunningStatusBarBackground"];
+        static Color RunningWindowBorderBrush => (Color)Application.Current.Resources["RunningWindowBorderBrush"];
+        static Color DefaultStatusBarBackground => (Color)Application.Current.Resources["DefaultStatusBarBackground"];
+        static Color DefaultWindowBorderBrush => (Color)Application.Current.Resources["DefaultWindowBorderBrush"];
+
+        private static bool _isRunning;
+        public static bool IsRunning
+        {
+            get => _isRunning;
+            set
+            {
+                _isRunning = value;
+                if (value)
+                {
+                    Application.Current.Resources["StatusBarBackground"] = new SolidColorBrush(RunningStatusBarBackground);
+                    Application.Current.Resources["EnvironmentMainWindowActiveDefaultBorder"] = new SolidColorBrush(RunningWindowBorderBrush);
+                }
+                else
+                {
+                    Application.Current.Resources["StatusBarBackground"] = new SolidColorBrush(DefaultStatusBarBackground);
+                    Application.Current.Resources["EnvironmentMainWindowActiveDefaultBorder"] = new SolidColorBrush(DefaultWindowBorderBrush);
+                }
+            }
+        }
+
+        public static void Update()
+        {
+            IsRunning = IsRunning;
+        }
+    }
+}
