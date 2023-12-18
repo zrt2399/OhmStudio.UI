8f95a8206f089eec76d90d4b06233ef38bcc1576
TheShy <zrt2399@163.com>
2023-12-18 17:57:13 +0800

    Nuget更新到1.1.0.2，DropShadowControl控件样式修正

--- OhmStudio.UI/Attaches/BorderAttach.cs
+++ OhmStudio.UI/Attaches/BorderAttach.cs
@@ -1,7 +1,4 @@
 ﻿using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using OhmStudio.UI.Converters;
 
 namespace OhmStudio.UI.Attaches
 {
@@ -9,10 +6,7 @@ namespace OhmStudio.UI.Attaches
     {
         public static readonly DependencyProperty CornerRadiusProperty =
             DependencyProperty.RegisterAttached("CornerRadius", typeof(CornerRadius), typeof(BorderAttach), new FrameworkPropertyMetadata(default(CornerRadius), FrameworkPropertyMetadataOptions.Inherits));
-
-        public static readonly DependencyProperty IsCircularProperty =
-            DependencyProperty.RegisterAttached("IsCircular", typeof(bool), typeof(BorderAttach), new PropertyMetadata(false, OnIsCircularChanged));
-
+ 
         public static void SetCornerRadius(DependencyObject element, CornerRadius value)
         {
             element.SetValue(CornerRadiusProperty, value);
@@ -21,45 +15,6 @@ namespace OhmStudio.UI.Attaches
         public static CornerRadius GetCornerRadius(DependencyObject element)
         {
             return (CornerRadius)element.GetValue(CornerRadiusProperty);
-        }
-
-        private static void OnIsCircularChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
-        {
-            if (sender is Border border)
-            {
-                if ((bool)e.NewValue)
-                {
-                    MultiBinding multiBinding = new MultiBinding
-                    {
-                        Converter = new BorderCircularConverter()
-                    };
-                    multiBinding.Bindings.Add(new Binding(FrameworkElement.ActualWidthProperty.Name)
-                    {
-                        Source = border
-                    });
-                    multiBinding.Bindings.Add(new Binding(FrameworkElement.ActualHeightProperty.Name)
-                    {
-                        Source = border
-                    });
-                    border.SetBinding(Border.CornerRadiusProperty, multiBinding);
-                }
-                else
-                {
-                    BindingOperations.ClearBinding(border, FrameworkElement.ActualWidthProperty);
-                    BindingOperations.ClearBinding(border, FrameworkElement.ActualHeightProperty);
-                    BindingOperations.ClearBinding(border, Border.CornerRadiusProperty);
-                }
-            }
-        }
-
-        public static void SetIsCircular(DependencyObject element, bool value)
-        {
-            element.SetValue(IsCircularProperty, value);
-        }
-
-        public static bool GetIsCircular(DependencyObject element)
-        {
-            return (bool)element.GetValue(IsCircularProperty);
-        }
+        } 
     }
 }
---
+++ OhmStudio.UI/Attaches/CornerRadiusAttach.cs
@@ -0,0 +1,71 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using OhmStudio.UI.Controls;
+using OhmStudio.UI.Converters;
+
+namespace OhmStudio.UI.Attaches
+{
+    public class CornerRadiusAttach
+    {
+        public static readonly DependencyProperty IsCircularProperty =
+            DependencyProperty.RegisterAttached("IsCircular", typeof(bool), typeof(BorderAttach), new PropertyMetadata(false, OnIsCircularChanged));
+
+        private static void OnIsCircularChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            DependencyProperty dependencyProperty = GetCornerRadiusProperty(sender);
+            if (dependencyProperty != null)
+            {
+                FrameworkElement frameworkElement = sender as FrameworkElement;
+                if ((bool)e.NewValue)
+                {
+                    MultiBinding multiBinding = new MultiBinding
+                    {
+                        Converter = new BorderCircularConverter()
+                    };
+                    multiBinding.Bindings.Add(new Binding(FrameworkElement.ActualWidthProperty.Name)
+                    {
+                        Source = frameworkElement
+                    });
+                    multiBinding.Bindings.Add(new Binding(FrameworkElement.ActualHeightProperty.Name)
+                    {
+                        Source = frameworkElement
+                    });
+                    frameworkElement.SetBinding(dependencyProperty, multiBinding);
+                }
+                else
+                {
+                    BindingOperations.ClearBinding(frameworkElement, FrameworkElement.ActualWidthProperty);
+                    BindingOperations.ClearBinding(frameworkElement, FrameworkElement.ActualHeightProperty);
+                    BindingOperations.ClearBinding(frameworkElement, dependencyProperty);
+                }
+            }
+        }
+
+        public static DependencyProperty GetCornerRadiusProperty(DependencyObject element)
+        {
+            if (element is Border)
+            {
+                return Border.CornerRadiusProperty;
+            }
+            else if (element is DropShadowControl)
+            {
+                return DropShadowControl.CornerRadiusProperty;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public static void SetIsCircular(DependencyObject element, bool value)
+        {
+            element.SetValue(IsCircularProperty, value);
+        }
+
+        public static bool GetIsCircular(DependencyObject element)
+        {
+            return (bool)element.GetValue(IsCircularProperty);
+        }
+    }
+}
