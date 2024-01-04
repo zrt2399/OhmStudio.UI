494dc35983053264b7390366c4d0e0b9949d806a
MarkWang <zrt2399@163.com>
2024-01-04 20:06:23 +0800

    增加RectangleCircularConverter

---
+++ OhmStudio.UI/Converters/RectangleCircularConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace OhmStudio.UI.Converters
+{
+    public class RectangleCircularConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.Length == 2 && values[0] is double num && values[1] is double num2)
+            {
+                if (num < double.Epsilon || num2 < double.Epsilon)
+                {
+                    return 0.0;
+                }
+
+                return Math.Min(num, num2) / 2.0;
+            }
+
+            return DependencyProperty.UnsetValue;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+} 
