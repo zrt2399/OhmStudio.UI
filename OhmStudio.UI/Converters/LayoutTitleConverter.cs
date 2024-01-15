4206231bb165cccd5eedf939b6f2d7f45372d8e9
TheShy <zrt2399@163.com>
2024-01-15 17:33:32 +0800

    增加LayoutTitleConverter

---
+++ OhmStudio.UI/Converters/LayoutTitleConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using AvalonDock.Controls;
+using AvalonDock.Layout;
+
+namespace OhmStudio.UI.Converters
+{
+    public class LayoutTitleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var floatingWindow = value as LayoutFloatingWindowControl;
+
+            var app = Application.Current?.MainWindow.Title + " - ";
+            if (floatingWindow.Model is LayoutDocumentFloatingWindow layoutDocumentFloatingWindow)
+            {
+                return app + layoutDocumentFloatingWindow.SinglePane.SelectedContent.Title;
+            }
+            else if (floatingWindow.Model is LayoutAnchorableFloatingWindow layoutAnchorableFloatingWindow)
+            {
+                return app + ((LayoutAnchorablePane)layoutAnchorableFloatingWindow.SinglePane).SelectedContent.Title;
+            }
+
+            return Application.Current?.MainWindow.Title;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
