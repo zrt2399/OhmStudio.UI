7eb577bdf5f76b897af69e4316075a66be123658
TheShy <zrt2399@163.com>
2024-02-19 17:48:27 +0800

    增加TextBoxAttachStyle，修复TextBoxAttach控件焦点获取问题

---
+++ OhmStudio.UI/Styles/VS/TextBoxAttachStyle.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace OhmStudio.UI.Styles.VS
+{
+    /// <summary>
+    /// TextBoxAttachStyle.xaml 的交互逻辑
+    /// </summary>
+    public partial class TextBoxAttachStyle : ResourceDictionary
+    {
+        private void ContentControl_GotFocus(object sender, RoutedEventArgs e)
+        {
+            var contentControl = (ContentControl)sender;
+            if (!e.Handled)
+            {
+                if (Equals(e.OriginalSource, contentControl))
+                {
+                    if (contentControl.Content is UIElement uIElement)
+                    {
+                        uIElement.Focus();
+                        e.Handled = true;
+                    }
+                }
+            }
+        }
+    }
+}
