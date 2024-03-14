3c4d24f6687eb8aa5c57ee996228f57d8acb4640
TheShy <zrt2399@163.com>
2024-03-14 16:23:34 +0800

    增加ListViewIndexConverter

---
+++ OhmStudio.UI/Converters/ListViewIndexConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace OhmStudio.UI.Converters
+{
+    public class ListViewIndexConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is not ListViewItem listViewItem)
+            {
+                return default;
+            }
+            ListView listView = ItemsControl.ItemsControlFromItemContainer(listViewItem) as ListView;
+            return listView.ItemContainerGenerator.IndexFromContainer(listViewItem) + (parameter == null ? 1 : System.Convert.ToInt32(parameter));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
