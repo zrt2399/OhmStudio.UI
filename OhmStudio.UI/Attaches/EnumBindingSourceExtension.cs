3b9afb9bdf331c3410d08204b7a55d4ee238a2ca
TheShy <zrt2399@163.com>
2024-03-14 16:23:23 +0800

    增加EnumBindingSourceExtension

---
+++ OhmStudio.UI/Attaches/EnumBindingSourceExtension.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Windows.Markup;
+
+namespace OhmStudio.UI.Attaches
+{
+    public class EnumBindingSourceExtension : MarkupExtension
+    {
+        private Type _enumType;
+        public Type EnumType
+        {
+            get => _enumType;
+            set
+            {
+                if (value != _enumType)
+                {
+                    if (value != null)
+                    {
+                        Type enumType = Nullable.GetUnderlyingType(value) ?? value;
+                        if (!enumType.IsEnum)
+                        {
+                            throw new ArgumentException("Type must be for an Enum.");
+                        }
+                    }
+                    _enumType = value;
+                }
+            }
+        }
+
+        public EnumBindingSourceExtension() { }
+
+        public EnumBindingSourceExtension(Type enumType)
+        {
+            EnumType = enumType;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (EnumType == null)
+            {
+                throw new InvalidOperationException("The EnumType must be specified.");
+            }
+
+            Type actualEnumType = Nullable.GetUnderlyingType(EnumType) ?? EnumType;
+            Array enumValues = Enum.GetValues(actualEnumType);
+
+            if (actualEnumType == EnumType)
+            {
+                return enumValues;
+            }
+
+            Array tempArray = Array.CreateInstance(actualEnumType, enumValues.Length + 1);
+            enumValues.CopyTo(tempArray, 1);
+            return tempArray;
+        }
+    }
+} 
