284733a943ff9dc6365bff4e7e816e10d6d05293
TheShy <zrt2399@163.com>
2024-04-15 17:31:22 +0800

    增加RadioButtonAttach

---
+++ OhmStudio.UI/Attaches/RadioButtonAttach.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace OhmStudio.UI.Attaches
+{
+    public class RadioButtonAttach
+    {
+        public static readonly DependencyProperty RadioButtonSizeProperty =
+            DependencyProperty.RegisterAttached("RadioButtonSize", typeof(double), typeof(RadioButtonAttach));
+
+        public static void SetRadioButtonSize(DependencyObject element, double value)
+        {
+            element.SetValue(RadioButtonSizeProperty, value);
+        }
+
+        public static double GetRadioButtonSize(DependencyObject element)
+        {
+            return (double)element.GetValue(RadioButtonSizeProperty);
+        }
+    }
+}
---
+++ OhmStudio.UI/Converters/HalfConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace OhmStudio.UI.Converters
+{
+    public class HalfConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (double.TryParse(value?.ToString(), out var result))
+            {
+                return result / 2;
+            }
+            else
+            {
+                return 0d;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
