11f50fb7beb9c7505e1dd3eed2f65cac81ba6353
TheShy <zrt2399@163.com>
2024-04-15 17:31:16 +0800

    增加CheckBoxAttach

---
+++ OhmStudio.UI/Attaches/CheckBoxAttach.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace OhmStudio.UI.Attaches
+{
+    public class CheckBoxAttach
+    {
+        public static readonly DependencyProperty CheckBoxSizeProperty =
+            DependencyProperty.RegisterAttached("CheckBoxSize", typeof(double), typeof(CheckBoxAttach));
+
+        public static void SetCheckBoxSize(DependencyObject element, double value)
+        {
+            element.SetValue(CheckBoxSizeProperty, value);
+        }
+
+        public static double GetCheckBoxSize(DependencyObject element)
+        {
+            return (double)element.GetValue(CheckBoxSizeProperty);
+        }
+    }
+}
