51126413bcff4810f39dc58a33526f2445cd899f
TheShy <zrt2399@163.com>
2024-04-18 17:43:00 +0800

    DataGridAttach优化

---
+++ OhmStudio.UI/Attaches/DataGridColumnAttach.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+
+namespace OhmStudio.UI.Attaches
+{
+    public class DataGridColumnAttach
+    {
+        public static readonly DependencyProperty IsIgnoreElementStyleProperty =
+            DependencyProperty.RegisterAttached("IsIgnoreElementStyle", typeof(bool), typeof(DataGridAttach));
+
+        public static bool GetIsIgnoreElementStyle(DependencyObject target)
+        {
+            return (bool)target.GetValue(IsIgnoreElementStyleProperty);
+        }
+
+        public static void SetIsIgnoreElementStyle(DependencyObject target, bool value)
+        {
+            target.SetValue(IsIgnoreElementStyleProperty, value);
+        }
+
+        public static readonly DependencyProperty IsIgnoreEditingElementStyleProperty =
+            DependencyProperty.RegisterAttached("IsIgnoreEditingElementStyle", typeof(bool), typeof(DataGridAttach));
+
+        public static bool GetIsIgnoreEditingElementStyle(DependencyObject target)
+        {
+            return (bool)target.GetValue(IsIgnoreEditingElementStyleProperty);
+        }
+
+        public static void SetIsIgnoreEditingElementStyle(DependencyObject target, bool value)
+        {
+            target.SetValue(IsIgnoreEditingElementStyleProperty, value);
+        }
+    }
+}
