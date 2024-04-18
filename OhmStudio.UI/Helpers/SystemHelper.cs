7ac939149d9dc30095cf7a6c7682f79e432de6a3
TheShy <zrt2399@163.com>
2024-04-18 17:43:09 +0800

    增加SystemHelper

---
+++ OhmStudio.UI/Helpers/SystemHelper.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Win32;
+
+namespace OhmStudio.UI.Helpers
+{
+    public class SystemHelper
+    {
+        private const string SkinTypeRegistryKeyName = "HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize";
+
+        private const string SkinTypeRegistryValueName = "AppsUseLightTheme";
+
+        public static bool DetermineIfInLightThemeMode
+        {
+            get
+            {
+                object value = Registry.GetValue(SkinTypeRegistryKeyName, SkinTypeRegistryValueName, "0");
+                if (value != null)
+                {
+                    return value.ToString() != "0";
+                }
+
+                return false;
+            }
+        }
+    } 
+}
