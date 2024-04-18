81de61e5a657e335e45155cd01f8972c5ac1c4d6
TheShy <zrt2399@163.com>
2024-04-18 17:45:40 +0800

    PasswordTextBox部分属性改为internal

--- OhmStudio.UI/Views/PasswordTextBox.xaml.cs
+++ OhmStudio.UI/Views/PasswordTextBox.xaml.cs
@@ -31,17 +31,17 @@ namespace OhmStudio.UI.Views
         /// <summary>
         /// 控制PasswordTextBox显示或者隐藏CheckBox，来控制是否可以显示和隐藏密码。
         /// </summary>
-        public Visibility CanShowPassword
+        public bool CanShowPassword
         {
-            get => (Visibility)GetValue(CanShowPasswordProperty);
+            get => (bool)GetValue(CanShowPasswordProperty);
             set => SetValue(CanShowPasswordProperty, value);
         }
 
-        // Using a DependencyProperty as the backing store for CanShowPasswordVisibility.  This enables animation,styling,binding,etc...
+        // Using a DependencyProperty as the backing store for CanShowPassword.  This enables animation,styling,binding,etc...
         public static readonly DependencyProperty CanShowPasswordProperty =
-            DependencyProperty.Register("CanShowPasswordVisibility", typeof(Visibility), typeof(PasswordTextBox), new PropertyMetadata(Visibility.Visible, (sender, e) =>
+            DependencyProperty.Register(nameof(CanShowPassword), typeof(bool), typeof(PasswordTextBox), new PropertyMetadata(true, (sender, e) =>
             {
-                if ((Visibility)e.NewValue != Visibility.Visible && sender is PasswordTextBox passwordTextBox)
+                if (!(bool)e.NewValue && sender is PasswordTextBox passwordTextBox)
                 {
                     passwordTextBox.IsChecked = false;
                 }
@@ -50,40 +50,40 @@ namespace OhmStudio.UI.Views
         /// <summary>
         /// 控制TextBox显示或者隐藏----TextBox来显示明文。
         /// </summary>
-        public Visibility TbVisibility
+        internal Visibility TbVisibility
         {
             get => (Visibility)GetValue(TbVisibilityProperty);
             set => SetValue(TbVisibilityProperty, value);
         }
 
         // Using a DependencyProperty as the backing store for TbVisibility.  This enables animation,styling,binding,etc...
-        public static readonly DependencyProperty TbVisibilityProperty =
+        internal static readonly DependencyProperty TbVisibilityProperty =
             DependencyProperty.Register("TbVisibility", typeof(Visibility), typeof(PasswordTextBox), new PropertyMetadata(Visibility.Collapsed));
 
         /// <summary>
         /// 控制PassworBox显示或者隐藏----PasswordBox控件来显密文。
         /// </summary>
-        public Visibility PwVisibility
+        internal Visibility PwVisibility
         {
             get => (Visibility)GetValue(PwVisibilityProperty);
             set => SetValue(PwVisibilityProperty, value);
         }
 
         // Using a DependencyProperty as the backing store for PwVisibility.  This enables animation,styling,binding,etc...
-        public static readonly DependencyProperty PwVisibilityProperty =
+        internal static readonly DependencyProperty PwVisibilityProperty =
             DependencyProperty.Register("PwVisibility", typeof(Visibility), typeof(PasswordTextBox));
 
         /// <summary>
         /// 和"眼睛"进行绑定。
         /// </summary>
-        public bool IsChecked
+        internal bool IsChecked
         {
             get => (bool)GetValue(IsCheckedProperty);
             set => SetValue(IsCheckedProperty, value);
         }
 
         // Using a DependencyProperty as the backing store for Check.  This enables animation,styling,binding,etc...
-        public static readonly DependencyProperty IsCheckedProperty =
+        internal static readonly DependencyProperty IsCheckedProperty =
             DependencyProperty.Register("IsChecked", typeof(bool), typeof(PasswordTextBox), new PropertyMetadata((sender, e) =>
             {
                 if (sender is PasswordTextBox passwordTextBox)
@@ -104,12 +104,12 @@ namespace OhmStudio.UI.Views
         /// <summary>
         /// 点击图标"x",使密码框清空。
         /// </summary>
-        public bool IsCleared
+        internal bool IsCleared
         {
             get => (bool)GetValue(IsClearedProperty);
             set => SetValue(IsClearedProperty, value);
         }
-        public static readonly DependencyProperty IsClearedProperty =
+        internal static readonly DependencyProperty IsClearedProperty =
             DependencyProperty.Register("IsCleared", typeof(bool), typeof(PasswordTextBox), new PropertyMetadata((sender, e) =>
             {
                 if (sender is PasswordTextBox passwordTextBox)
@@ -121,12 +121,12 @@ namespace OhmStudio.UI.Views
         /// <summary>
         /// 控制显示符号"x"。
         /// </summary>
-        public Visibility ClearVisibility
+        internal Visibility ClearVisibility
         {
             get => (Visibility)GetValue(ClearVisibilityProperty);
             set => SetValue(ClearVisibilityProperty, value);
         }
-        public static readonly DependencyProperty ClearVisibilityProperty =
+        internal static readonly DependencyProperty ClearVisibilityProperty =
             DependencyProperty.Register("ClearVisibility", typeof(Visibility), typeof(PasswordTextBox), new PropertyMetadata(Visibility.Collapsed));
 
         /// <summary>
