ac91fe06e22eb98b58907a92053d335c37536121
MarkWang <zrt2399@163.com>
2024-04-27 03:20:32 +0800

    ImageAttach增加Source属性

--- OhmStudio.UI/Attaches/ImageAttach.cs
+++ OhmStudio.UI/Attaches/ImageAttach.cs
@@ -1,6 +1,8 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace OhmStudio.UI.Attaches
 {
@@ -8,6 +10,16 @@ namespace OhmStudio.UI.Attaches
     {
         public static readonly DependencyProperty SourceFailedProperty =
             DependencyProperty.RegisterAttached("SourceFailed", typeof(ImageSource), typeof(ImageAttach), new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.Inherits, OnSourceFailedChanged));
+
+        public static void SetSourceFailed(DependencyObject element, ImageSource value)
+        {
+            element.SetValue(SourceFailedProperty, value);
+        }
+
+        public static ImageSource GetSourceFailed(DependencyObject element)
+        {
+            return (ImageSource)element.GetValue(SourceFailedProperty);
+        }
 
         private static void OnSourceFailedChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
         {
@@ -33,14 +45,32 @@ namespace OhmStudio.UI.Attaches
             }
         }
 
-        public static void SetSourceFailed(DependencyObject element, ImageSource value)
+        public static readonly DependencyProperty SourceProperty =
+           DependencyProperty.RegisterAttached("Source", typeof(Uri), typeof(ImageAttach), new PropertyMetadata((sender, e) =>
+           {
+               if (sender is Image image && e.NewValue is Uri uri)
+               {
+                   image.Source = null;
+                   var bitmapImage = new BitmapImage();
+                   //bitmapImage.CreateOptions = BitmapCreateOptions.None;
+                   bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                   //bitmapImage.DecodePixelWidth = 100;
+                   bitmapImage.BeginInit();
+                   bitmapImage.UriSource = uri;
+                   bitmapImage.EndInit();
+                   //bitmapImage.Freeze();
+                   image.Source = bitmapImage;
+               }
+           }));
+
+        public static void SetSource(DependencyObject element, Uri value)
         {
-            element.SetValue(SourceFailedProperty, value);
+            element.SetValue(SourceProperty, value);
         }
 
-        public static ImageSource GetSourceFailed(DependencyObject element)
+        public static Uri GetSource(DependencyObject element)
         {
-            return (ImageSource)element.GetValue(SourceFailedProperty);
+            return (Uri)element.GetValue(SourceProperty);
         }
     }
-} 
+}
