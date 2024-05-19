da5a82e36faa1820c98fbab27fa9932b958e0618
MarkWang <zrt2399@163.com>
2024-05-19 08:26:11 +0800

    新增MouseDownAttach

---
+++ OhmStudio.UI/Attaches/MouseDownAttach.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace OhmStudio.UI.Attaches
+{
+    [Flags]
+    public enum MouseClickMode
+    {
+        None = 0,
+        Left = 1,
+        Middle = 1 << 1,
+        Right = 1 << 2,
+        XButton1 = 1 << 3,
+        XButton2 = 1 << 4
+    }
+
+    public class MouseDownAttach
+    {
+        public static readonly DependencyProperty IgnoreElementProperty =
+            DependencyProperty.RegisterAttached("IgnoreElement", typeof(string),
+                typeof(MouseDownAttach));
+
+        public static string GetIgnoreElement(DependencyObject element)
+        {
+            return (string)element.GetValue(IgnoreElementProperty);
+        }
+
+        public static void SetIgnoreElement(DependencyObject element, string value)
+        {
+            element.SetValue(IgnoreElementProperty, value);
+        }
+
+        public static readonly DependencyProperty MouseClickModeProperty =
+            DependencyProperty.RegisterAttached("MouseClickMode", typeof(MouseClickMode),
+                typeof(MouseDownAttach), new PropertyMetadata(MouseClickMode.Left | MouseClickMode.Right));
+
+        public static MouseClickMode GetMouseClickMode(DependencyObject element)
+        {
+            return (MouseClickMode)element.GetValue(MouseClickModeProperty);
+        }
+
+        public static void SetMouseClickMode(DependencyObject element, MouseClickMode value)
+        {
+            element.SetValue(MouseClickModeProperty, value);
+        }
+
+        public static readonly DependencyProperty MouseClickCountProperty =
+            DependencyProperty.RegisterAttached("MouseClickCount", typeof(int),
+                typeof(MouseDownAttach), new PropertyMetadata(1));
+
+        public static int GetMouseClickCount(DependencyObject element)
+        {
+            return (int)element.GetValue(MouseClickCountProperty);
+        }
+
+        public static void SetMouseClickCount(DependencyObject element, int value)
+        {
+            element.SetValue(MouseClickCountProperty, value);
+        }
+
+        public static readonly DependencyProperty CommandProperty =
+            DependencyProperty.RegisterAttached("Command", typeof(ICommand),
+                typeof(MouseDownAttach), new FrameworkPropertyMetadata(OnCommandChanged));
+
+        private static void OnCommandChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (sender is not FrameworkElement frameworkElement)
+            {
+                return;
+            }
+
+            frameworkElement.PreviewMouseDown -= FrameworkElement_PreviewMouseDown;
+
+            if (e.NewValue != null)
+            {
+                frameworkElement.PreviewMouseDown += FrameworkElement_PreviewMouseDown;
+            }
+        }
+
+        private static void FrameworkElement_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (sender is FrameworkElement frameworkElement && e.ClickCount >= GetMouseClickCount(frameworkElement))
+            {
+                var ignoreElement = GetIgnoreElement(frameworkElement);
+                if (!string.IsNullOrEmpty(ignoreElement))
+                {
+                    var types = ignoreElement.Split(',');
+                    foreach (var item in types)
+                    {
+                        if (e.OriginalSource.GetType().Name == item)
+                        {
+                            return;
+                        }
+                    }
+                }
+                var command = GetCommand(frameworkElement);
+                var commandParameter = GetCommandParameter(frameworkElement);
+                var mouseClickMode = GetMouseClickMode(frameworkElement);
+                if ((mouseClickMode & MouseClickMode.Left) != 0 && e.ChangedButton == MouseButton.Left)
+                {
+                    command?.Execute(commandParameter);
+                }
+                if ((mouseClickMode & MouseClickMode.Right) != 0 && e.ChangedButton == MouseButton.Right)
+                {
+                    command?.Execute(commandParameter);
+                }
+                if ((mouseClickMode & MouseClickMode.Middle) != 0 && e.ChangedButton == MouseButton.Middle)
+                {
+                    command?.Execute(commandParameter);
+                }
+                if ((mouseClickMode & MouseClickMode.XButton1) != 0 && e.ChangedButton == MouseButton.XButton1)
+                {
+                    command?.Execute(commandParameter);
+                }
+                if ((mouseClickMode & MouseClickMode.XButton2) != 0 && e.ChangedButton == MouseButton.XButton2)
+                {
+                    command?.Execute(commandParameter);
+                }
+            }
+        }
+
+        public static ICommand GetCommand(DependencyObject element)
+        {
+            return (ICommand)element.GetValue(CommandProperty);
+        }
+
+        public static void SetCommand(DependencyObject element, ICommand value)
+        {
+            element.SetValue(CommandProperty, value);
+        }
+
+        public static readonly DependencyProperty CommandParameterProperty =
+           DependencyProperty.RegisterAttached("CommandParameter", typeof(object), typeof(MouseDownAttach));
+
+        public static object GetCommandParameter(DependencyObject element)
+        {
+            return element.GetValue(CommandParameterProperty);
+        }
+
+        public static void SetCommandParameter(DependencyObject element, object value)
+        {
+            element.SetValue(CommandParameterProperty, value);
+        }
+    }
+}
