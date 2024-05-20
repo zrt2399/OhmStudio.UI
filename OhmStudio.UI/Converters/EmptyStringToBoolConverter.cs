775db1aecf4b40723429bfd283fdcf4d564b3d21
MarkWang <zrt2399@163.com>
2024-05-21 00:04:05 +0800

    增加EmptyStringToBoolConverter

---
+++ OhmStudio.UI/Converters/EmptyStringToBoolConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace OhmStudio.UI.Converters
+{
+    public class EmptyStringToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+           return string.IsNullOrEmpty(value?.ToString());
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class EmptyStringToNotBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !string.IsNullOrEmpty(value?.ToString());
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+} 
