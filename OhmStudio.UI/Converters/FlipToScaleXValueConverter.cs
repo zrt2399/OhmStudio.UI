0db0ceb1d3e83dd2d4a6363267bf90ec8fd5d33f
MarkWang <zrt2399@163.com>
2024-05-25 05:37:03 +0800

    添加FlipToScaleXValueConverter

---
+++ OhmStudio.UI/Converters/FlipToScaleXValueConverter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using OhmStudio.UI.Controls;
+
+namespace OhmStudio.UI.Converters
+{
+    public class FlipToScaleXValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is PackIconFlipOrientation flip)
+            {
+                var scaleX = flip == PackIconFlipOrientation.Horizontal || flip == PackIconFlipOrientation.Both ? -1 : 1;
+                return scaleX;
+            }
+
+            return 1;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class FlipToScaleYValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is PackIconFlipOrientation flip)
+            {
+                var scaleY = flip == PackIconFlipOrientation.Vertical || flip == PackIconFlipOrientation.Both ? -1 : 1;
+                return scaleY;
+            }
+
+            return 1;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+} 
