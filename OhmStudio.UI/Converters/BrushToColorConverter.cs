18f10da9d952c1047475c0785c5784af5bb777c7
MarkWang <zrt2399@163.com>
2024-06-02 01:12:46 +0800

    增加ColorToBrushConverter

--- OhmStudio.UI/Converters/BrushToColorConverter.cs
+++ OhmStudio.UI/Converters/BrushToColorConverter.cs
@@ -21,4 +21,21 @@ namespace OhmStudio.UI.Converters
             throw new NotImplementedException();
         }
     }
+
+    public class ColorToBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Color color)
+            {
+                return new SolidColorBrush(color);
+            }
+            return new SolidColorBrush(Brushes.Transparent.Color);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
