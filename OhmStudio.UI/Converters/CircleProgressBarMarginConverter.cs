e55819f6ebbc5bd58238722b7df6a47d429769b5
MarkWang <zrt2399@163.com>
2024-06-02 01:13:07 +0800

    增加CircleProgressBarMarginConverter

---
+++ OhmStudio.UI/Converters/CircleProgressBarMarginConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows;
+
+namespace OhmStudio.UI.Converters
+{
+    public class CircleProgressBarMarginConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values != null && values.Length >= 2)
+            {
+                return new Thickness(Math.Max(System.Convert.ToDouble(values[0]), System.Convert.ToDouble(values[1])) / 2);
+            }
+            return new Thickness(0);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
