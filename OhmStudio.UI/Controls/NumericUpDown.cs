8f447be33b74d1a6a65d3bde83ebbe2aea78e054
MarkWang <zrt2399@163.com>
2024-06-16 06:27:31 +0800

    添加NumericUpDown

---
+++ OhmStudio.UI/Controls/NumericUpDown.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace OhmStudio.UI.Controls
+{
+    public class NumericUpDown : Control
+    {
+        static NumericUpDown()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(NumericUpDown), new FrameworkPropertyMetadata(typeof(NumericUpDown)));
+        }
+    }
+} 
