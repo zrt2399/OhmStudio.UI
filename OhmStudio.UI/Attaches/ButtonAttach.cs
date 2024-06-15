f891a2d1190a505c628286cb5ae3eae5fde75695
MarkWang <zrt2399@163.com>
2024-06-16 06:26:26 +0800

    增加ButtonAttach

---
+++ OhmStudio.UI/Attaches/ButtonAttach.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace OhmStudio.UI.Attaches
+{
+    public class ButtonAttach
+    {
+        public static readonly DependencyProperty PressedBackgroundProperty =
+            DependencyProperty.RegisterAttached("PressedBackground", typeof(Brush), typeof(ButtonAttach));
+
+        public static void SetPressedBackground(DependencyObject element, Brush value)
+        {
+            element.SetValue(PressedBackgroundProperty, value);
+        }
+
+        public static Brush GetPressedBackground(DependencyObject element)
+        {
+            return (Brush)element.GetValue(PressedBackgroundProperty);
+        }
+
+        public static readonly DependencyProperty PressedBorderBrushProperty =
+            DependencyProperty.RegisterAttached("PressedBorderBrush", typeof(Brush), typeof(ButtonAttach));
+
+        public static void SetPressedBorderBrush(DependencyObject element, Brush value)
+        {
+            element.SetValue(PressedBorderBrushProperty, value);
+        }
+
+        public static Brush GetPressedBorderBrush(DependencyObject element)
+        {
+            return (Brush)element.GetValue(PressedBorderBrushProperty);
+        }
+    }
+} 
