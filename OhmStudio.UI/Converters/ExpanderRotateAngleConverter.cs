1ebcf9ff9facdef274a8a904f0ce33adda1317ae
MarkWang <zrt2399@163.com>
2024-09-01 01:15:48 +0800

    Expander添加展开和收缩动画

---
+++ OhmStudio.UI/Converters/ExpanderRotateAngleConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace OhmStudio.UI.Converters
+{
+    public class ExpanderRotateAngleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double factor = 1.0;
+            if (double.TryParse(parameter?.ToString(), out var result))
+            {
+                factor = result;
+            }
+            return value switch
+            {
+                ExpandDirection.Left => 90 * factor,
+                ExpandDirection.Right => -90 * factor,
+                _ => 0
+            };
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
