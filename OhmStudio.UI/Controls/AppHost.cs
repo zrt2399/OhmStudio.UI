19ca70002643742f47c4ff06e901d5f8b6c09182
MarkWang <zrt2399@163.com>
2024-09-03 00:20:16 +0800

    添加AppHost

---
+++ OhmStudio.UI/Controls/AppHost.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Forms.Integration;
+
+namespace OhmStudio.UI.Controls
+{
+    public class AppHost : Control
+    {
+        private Process _process = null;
+        private System.Windows.Forms.Panel PART_Host;
+
+        private const int WM_ACTIVATE = 0x0006;
+        private readonly IntPtr WA_ACTIVE = new IntPtr(1);
+        private readonly IntPtr WA_INACTIVE = new IntPtr(0);
+
+        public static readonly DependencyProperty ExePathProperty =
+            DependencyProperty.Register(nameof(ExePath), typeof(string), typeof(AppHost), new PropertyMetadata(string.Empty, (sender, e) =>
+            {
+                if ( sender is AppHost appHost)
+                {
+                    appHost.StartAndEmbedProcess();
+                }
+            }));
+
+        public static readonly DependencyProperty ArgumentsProperty =
+            DependencyProperty.Register(nameof(Arguments), typeof(string), typeof(AppHost), new PropertyMetadata(string.Empty));
+
+        public static readonly DependencyProperty IsSingleInstanceProperty =
+            DependencyProperty.Register(nameof(IsSingleInstance), typeof(bool), typeof(AppHost), new PropertyMetadata(true));
+
+        public string ExePath
+        {
+            get => (string)GetValue(ExePathProperty);
+            set => SetValue(ExePathProperty, value);
+        }
+
+        public string Arguments
+        {
+            get => (string)GetValue(ArgumentsProperty);
+            set => SetValue(ArgumentsProperty, value);
+        }
+
+        public bool IsSingleInstance
+        {
+            get => (bool)GetValue(IsSingleInstanceProperty);
+            set => SetValue(IsSingleInstanceProperty, value);
+        }
+
+        public IntPtr EmbededWindowHandle => _process?.MainWindowHandle ?? IntPtr.Zero;
+
+        public override void OnApplyTemplate()
+        {
+            if (PART_Host != null)
+            {
+                PART_Host.Resize -= PART_Host_Resize;
+            }
+            base.OnApplyTemplate();
+            PART_Host = (GetTemplateChild("PART_Host") as WindowsFormsHost).Child as System.Windows.Forms.Panel;
+            PART_Host.Resize += PART_Host_Resize;
+        }
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+        [DllImport("user32.dll", EntryPoint = "SetWindowLongW")]
+        private static extern uint SetWindowLong(IntPtr hwnd, int nIndex, uint newLong);
+        [DllImport("user32.dll", EntryPoint = "GetWindowLongW")]
+        private static extern int GetWindowLong(IntPtr hwnd, int nIndex);
+        [DllImport("User32.dll")]
+        private static extern bool MoveWindow(IntPtr handle, int x, int y, int width, int height, bool redraw);
+
+        [DllImport("user32.dll")]
+        private static extern int SendMessage(IntPtr hWnd, int msg, IntPtr wParam, IntPtr lParam);
+
+        private void PART_Host_Resize(object sender, EventArgs e)
+        {
+            SetBounds();
+        }
+
+        public void ActivateWindow()
+        {
+            if (_process == null || EmbededWindowHandle == IntPtr.Zero)
+            {
+                return;
+            }
+
+            SendMessage(EmbededWindowHandle, WM_ACTIVATE, WA_ACTIVE, IntPtr.Zero);
+        }
+
+        public void SetBounds()
+        {
+            SetBounds(PART_Host.Width, PART_Host.Height);
+        }
+
+        public void SetBounds(int width, int height)
+        {
+            if (_process == null || EmbededWindowHandle == IntPtr.Zero)
+            {
+                return;
+            }
+
+            if (width <= 0 || height <= 0)
+            {
+                return;
+            }
+
+            MoveWindow(EmbededWindowHandle, 0, 0, width, height, true);
+
+            ActivateWindow();//激活
+        }
+
+        private static Process GetRunningProcess(string processName, string targetExePath)
+        {
+            // 获取所有与指定名称匹配的进程
+            Process[] processes = Process.GetProcessesByName(processName);
+
+            foreach (Process process in processes)
+            {
+                var exePath = process.MainModule?.FileName;
+
+                // 比较路径，确保是目标的 .exe 文件
+                if (exePath?.Equals(targetExePath, StringComparison.OrdinalIgnoreCase) == true)
+                {
+                    return process;
+                }
+            }
+
+            return null;
+        }
+
+        public async void StartAndEmbedProcess()
+        {
+            if (PART_Host == null)
+            {
+                Loaded += AppHost_Loaded;
+                return;
+            }
+            string processPath = ExePath;
+            var runningProcess = GetRunningProcess(Path.GetFileNameWithoutExtension(processPath), processPath);
+            if (runningProcess != null && IsSingleInstance)
+            {
+                KillProcess(runningProcess);
+            }
+            ProcessStartInfo processStartInfo = new ProcessStartInfo(processPath);
+            //info.WindowStyle = ProcessWindowStyle.Minimized;//默认最大化，不弹出界面。
+            //info.Arguments = $"-popupwindow";//Unity的命令行参数
+            processStartInfo.Arguments = Arguments;
+            processStartInfo.CreateNoWindow = true;
+
+            _process = Process.Start(processStartInfo);
+
+            if (_process == null)
+            {
+                return;
+            }
+
+            _process.WaitForInputIdle();
+            var hostHandle = PART_Host.Handle;
+            await Task.Factory.StartNew(() =>
+            {
+                for (int i = 0; i < 100 && EmbededWindowHandle == IntPtr.Zero; i++)
+                {
+                    Thread.Sleep(10);
+                }
+                if (EmbededWindowHandle != IntPtr.Zero)
+                {
+                    if (!EmbedApp(hostHandle, EmbededWindowHandle))
+                    {
+                        KillProcess();
+                    }
+                }
+            }, TaskCreationOptions.LongRunning);
+        }
+
+        private void AppHost_Loaded(object sender, RoutedEventArgs e)
+        {
+            Loaded -= AppHost_Loaded;
+            StartAndEmbedProcess();
+        }
+
+        private const int GWL_STYLE = -16;
+        private const int WS_CAPTION = 0x00C00000;
+        //private const int WS_THICKFRAME = 0x00040000;
+        private const int WS_BORDER = 0x00800000;
+
+        /// <summary>
+        /// 将外进程嵌入到当前程序。
+        /// </summary>
+        /// <param name="process"></param>
+        private bool EmbedApp(IntPtr panelHwnd, IntPtr processHwnd)
+        {
+            //是否嵌入成功标志，用作返回值
+            var isEmbedSuccess = false;
+
+            var wndStyle = GetWindowLong(processHwnd, GWL_STYLE);
+            wndStyle &= ~WS_BORDER;
+            wndStyle &= ~WS_CAPTION;
+            SetWindowLong(processHwnd, GWL_STYLE, (uint)wndStyle);
+
+            if (processHwnd != (IntPtr)0 && panelHwnd != (IntPtr)0)
+            {
+                //把本窗口句柄与目标窗口句柄关联起来
+                var setTime = 0;
+                while (!isEmbedSuccess && setTime < 50)
+                {
+                    // Put it into this form
+                    isEmbedSuccess = SetParent(processHwnd, panelHwnd) != IntPtr.Zero;
+                    Thread.Sleep(10);
+                    setTime++;
+                }
+
+                // Remove border and whatnot
+                //Win32Api.SetWindowLong(processHwnd, Win32Api.GWL_STYLE, Win32Api.WS_CHILDWINDOW | Win32Api.WS_CLIPSIBLINGS | Win32Api.WS_CLIPCHILDREN | Win32Api.WS_VISIBLE);
+
+                SetBounds();
+
+                //Move the window to overlay it on this window
+                //Win32Api.MoveWindow(EmbededWindowHandle, 0, 0, (int)ActualWidth, (int)ActualHeight, true);
+            }
+
+            return isEmbedSuccess;
+        }
+
+        private void KillProcess(Process process)
+        {
+            if (process != null && !process.HasExited)
+            {
+                process.Kill();
+            }
+        }
+
+        public void KillProcess()
+        {
+            KillProcess(_process);
+            _process = null;
+        }
+    }
+}
