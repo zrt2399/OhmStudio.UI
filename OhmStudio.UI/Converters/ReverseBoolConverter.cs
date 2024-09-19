c684c7c81fb099ec4a0989afd19ea04ea24bc5fa
MarkWang <zrt2399@163.com>
2024-09-20 03:50:49 +0800

    添加ReverseBoolConverter

---
+++ OhmStudio.UI/Converters/ReverseBoolConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace OhmStudio.UI.Converters
+{
+    public class ReverseBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is bool boolValue ? !boolValue : Binding.DoNothing;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is bool boolValue ? !boolValue : Binding.DoNothing;
+        }
+    }
+}
