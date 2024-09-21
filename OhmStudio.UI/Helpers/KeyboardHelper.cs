5a92f51bdfb4dc77dedb08b95487146b5c59268b
MarkWang <zrt2399@163.com>
2024-09-22 06:47:14 +0800

    添加KeyboardHelper

---
+++ OhmStudio.UI/Helpers/KeyboardHelper.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.InteropServices;
+using System.Windows.Input;
+
+namespace OhmStudio.UI.Helpers
+{
+    public static class KeyboardHelper
+    {
+        [DllImport("user32.dll")]
+        internal static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, uint dwExtraInfo);
+
+        public static bool IsCtrlKeyDown => Keyboard.IsKeyDown(Key.LeftCtrl) || Keyboard.IsKeyDown(Key.RightCtrl);
+
+        public static bool IsShiftKeyDown => Keyboard.IsKeyDown(Key.LeftShift) || Keyboard.IsKeyDown(Key.RightShift);
+
+        public static bool IsAltKeyDown => Keyboard.IsKeyDown(Key.LeftAlt) || Keyboard.IsKeyDown(Key.RightAlt);
+
+        public static void KeyboardDown(byte keyCode)
+        {
+            keybd_event(keyCode, 0, 0, 0);// 模拟按下键
+        }
+
+        public static void KeyboardUp(byte keyCode)
+        {
+            keybd_event(keyCode, 0, 2, 0);// 模拟释放键
+        }
+    }
+}
