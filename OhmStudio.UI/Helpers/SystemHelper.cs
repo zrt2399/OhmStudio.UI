c68f9d55fbd8a558cb7e3d6ce7559f0afe676da5
MarkWang <zrt2399@163.com>
2024-09-28 06:37:40 +0800

    SystemHelper添加鼠标键盘未操作时间

--- OhmStudio.UI/Helpers/SystemHelper.cs
+++ OhmStudio.UI/Helpers/SystemHelper.cs
@@ -1,12 +1,46 @@
-﻿using Microsoft.Win32;
+﻿using System.Runtime.InteropServices;
+using Microsoft.Win32;
 
 namespace OhmStudio.UI.Helpers
 {
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct LASTINPUTINFO
+    {
+        public uint cbSize;
+        public uint dwTime;
+    }
+
     public class SystemHelper
     {
         private const string SkinTypeRegistryKeyName = "HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize";
 
         private const string SkinTypeRegistryValueName = "AppsUseLightTheme";
+
+        [DllImport("kernel32.dll")]
+        internal static extern ulong GetTickCount64();
+
+        [DllImport("user32.dll")]
+        internal static extern bool GetLastInputInfo(ref LASTINPUTINFO plii);
+
+        public static ulong TickCount64 => GetTickCount64();
+
+        public static ulong IdleTimeInMilliseconds
+        {
+            get
+            {
+                LASTINPUTINFO lASTINPUTINFO = new LASTINPUTINFO();
+                lASTINPUTINFO.cbSize = (uint)Marshal.SizeOf(lASTINPUTINFO);
+                return GetLastInputInfo(ref lASTINPUTINFO) ? TickCount64 - lASTINPUTINFO.dwTime : 0;
+            }
+        }
+
+        public static double IdleTimeInSeconds => IdleTimeInMilliseconds / 1000d;
+
+        public static double IdleTimeInMinutes => IdleTimeInSeconds / 60;
+
+        public static double IdleTimeInHours => IdleTimeInMinutes / 60;
+
+        public static double IdleTimeInDays => IdleTimeInHours / 24;
 
         public static bool DetermineIfInLightThemeMode
         {
@@ -21,5 +55,5 @@ namespace OhmStudio.UI.Helpers
                 return false;
             }
         }
-    } 
+    }
 }
