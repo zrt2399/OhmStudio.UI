5a35baaa0d4198ad262ee41a49d742fff0959b02
TheShy <TheShy@qq.com>
2024-10-22 18:33:26 +0800

    Selector添加ScrollToEnd附加属性

--- OhmStudio.UI/Attaches/SelectorAttach.cs
+++ OhmStudio.UI/Attaches/SelectorAttach.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections;
+using System.Collections.Specialized;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
+using OhmStudio.UI.PublicMethods;
 
 namespace OhmStudio.UI.Attaches
 {
@@ -33,6 +35,19 @@ namespace OhmStudio.UI.Attaches
             obj.SetValue(SelectedItemsAttachProperty, value);
         }
 
+        public static readonly DependencyProperty IsScrollToBottomProperty =
+            DependencyProperty.RegisterAttached("IsScrollToBottom", typeof(bool), typeof(SelectorAttach), new PropertyMetadata(false, OnIsScrollToBottomChanged));
+
+        public static void SetIsScrollToBottom(DependencyObject obj, bool value)
+        {
+            obj.SetValue(IsScrollToBottomProperty, value);
+        }
+
+        public static bool GetIsScrollToBottom(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(IsScrollToBottomProperty);
+        }
+
         private static void SelectedChangedCallBack(DependencyObject obj, DependencyPropertyChangedEventArgs e)
         {
             if (obj is Selector selector)
@@ -62,6 +77,30 @@ namespace OhmStudio.UI.Attaches
                 }
                 //BindingExpression bind = selector.GetBindingExpression(SelectedItemsProperty);
                 //bind?.UpdateSource();
+            }
+        }
+
+        private static void OnIsScrollToBottomChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is Selector selector && selector.ItemsSource is INotifyCollectionChanged notifyCollectionChanged)
+            {
+                if ((bool)e.NewValue)
+                {
+                    notifyCollectionChanged.CollectionChanged += SelectorAttach_CollectionChanged;
+                }
+                else
+                {
+                    notifyCollectionChanged.CollectionChanged -= SelectorAttach_CollectionChanged;
+                }
+            }
+        }
+
+        private static void SelectorAttach_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            var selector = (Selector)sender;
+            if (e.Action == NotifyCollectionChangedAction.Add && selector.Items.Count > 0)
+            {
+                selector.ScrollToEnd();
             }
         }
     }
