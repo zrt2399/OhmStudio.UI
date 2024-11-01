633b60cae3d721db755cf29ebadef9ff1e6bc44d
MarkWang <zrt2399@163.com>
2024-11-02 06:19:12 +0800

    添加PlaceholderHorizontalAlignmentConverter

--- OhmStudio.UI/Converters/BoolToCollapsedConverter.cs
+++ OhmStudio.UI/Converters/BoolToCollapsedConverter.cs
@@ -6,38 +6,6 @@ using System.Windows.Data;
 namespace OhmStudio.UI.Converters
 {
     [ValueConversion(typeof(bool), typeof(Visibility))]
-    public class BoolToVisibilityConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            if (value is bool result && result)
-            {
-                return Visibility.Visible;
-            }
-            if (parameter is Visibility visibilityParam)
-            {
-                return visibilityParam;
-            }
-            return Visibility.Collapsed;
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            if (value is not Visibility)
-            {
-                throw new ArgumentException("Invalid argument type. Expected argument: Visibility.", nameof(value));
-            }
-
-            if (targetType != typeof(bool))
-            {
-                throw new ArgumentException("Invalid return type. Expected type: bool", nameof(targetType));
-            }
-
-            return (Visibility)value == Visibility.Visible;
-        }
-    }
-
-    [ValueConversion(typeof(bool), typeof(Visibility))]
     public class BoolToCollapsedConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
---
+++ OhmStudio.UI/Converters/PlaceholderHorizontalAlignmentConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace OhmStudio.UI.Converters
+{
+    public class PlaceholderHorizontalAlignmentConverter : IValueConverter
+    { 
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Control control)
+            {
+                return control.HorizontalContentAlignment;
+            }
+            return Binding.DoNothing;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
