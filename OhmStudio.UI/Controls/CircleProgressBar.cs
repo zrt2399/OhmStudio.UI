b726a28f127692403f794853ed927cb2e4c300e8
MarkWang <zrt2399@163.com>
2024-11-25 00:18:59 +0800

    CircleProgressBar添加Content属性

--- OhmStudio.UI/Controls/CircleProgressBar.cs
+++ OhmStudio.UI/Controls/CircleProgressBar.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Markup;
 using System.Windows.Media.Animation;
 
 namespace OhmStudio.UI.Controls
@@ -8,6 +10,7 @@ namespace OhmStudio.UI.Controls
     /// <summary>
     /// 圆形进度条。
     /// </summary>
+    [ContentProperty(nameof(Content))]
     public partial class CircleProgressBar : ProgressBar
     {
         public CircleProgressBar()
@@ -20,14 +23,41 @@ namespace OhmStudio.UI.Controls
             DefaultStyleKeyProperty.OverrideMetadata(typeof(CircleProgressBar), new FrameworkPropertyMetadata(typeof(CircleProgressBar)));
         }
 
-        public string Title
+        public static readonly DependencyProperty ContentProperty =
+            DependencyProperty.Register(nameof(Content), typeof(object), typeof(CircleProgressBar));
+
+        public static readonly DependencyProperty ContentTemplateProperty =
+            DependencyProperty.Register(nameof(ContentTemplate), typeof(DataTemplate), typeof(CircleProgressBar));
+
+        public static readonly DependencyProperty ContentTemplateSelectorProperty =
+            DependencyProperty.Register(nameof(ContentTemplateSelector), typeof(DataTemplateSelector), typeof(CircleProgressBar));
+
+        public static readonly DependencyProperty BrushStrokeThicknessProperty =
+            DependencyProperty.Register(nameof(BrushStrokeThickness), typeof(double), typeof(CircleProgressBar), new PropertyMetadata(1.0));
+
+        public static readonly DependencyProperty StrokeThicknessProperty =
+            DependencyProperty.Register(nameof(StrokeThickness), typeof(double), typeof(CircleProgressBar), new PropertyMetadata(10.0));
+
+        public static readonly DependencyProperty AngleProperty =
+            DependencyProperty.Register(nameof(Angle), typeof(double), typeof(CircleProgressBar), new PropertyMetadata(0.0));
+
+        public object Content
         {
-            get => (string)GetValue(TitleProperty);
-            set => SetValue(TitleProperty, value);
+            get => GetValue(ContentProperty);
+            set => SetValue(ContentProperty, value);
         }
 
-        public static readonly DependencyProperty TitleProperty =
-            DependencyProperty.Register("Title", typeof(string), typeof(CircleProgressBar), new PropertyMetadata(string.Empty));
+        public DataTemplate ContentTemplate
+        {
+            get => (DataTemplate)GetValue(ContentTemplateProperty);
+            set => SetValue(ContentTemplateProperty, value);
+        }
+
+        public DataTemplateSelector ContentTemplateSelector
+        {
+            get => (DataTemplateSelector)GetValue(ContentTemplateSelectorProperty);
+            set => SetValue(ContentTemplateSelectorProperty, value);
+        }
 
         public double Angle
         {
@@ -35,26 +65,19 @@ namespace OhmStudio.UI.Controls
             set => SetValue(AngleProperty, value);
         }
 
-        public static readonly DependencyProperty AngleProperty =
-            DependencyProperty.Register("Angle", typeof(double), typeof(CircleProgressBar), new PropertyMetadata(0.0));
-
+        [TypeConverter(typeof(LengthConverter))]
         public double StrokeThickness
         {
             get => (double)GetValue(StrokeThicknessProperty);
             set => SetValue(StrokeThicknessProperty, value);
         }
 
-        public static readonly DependencyProperty StrokeThicknessProperty =
-            DependencyProperty.Register("StrokeThickness", typeof(double), typeof(CircleProgressBar), new PropertyMetadata(10.0));
-
+        [TypeConverter(typeof(LengthConverter))]
         public double BrushStrokeThickness
         {
             get => (double)GetValue(BrushStrokeThicknessProperty);
             set => SetValue(BrushStrokeThicknessProperty, value);
         }
-
-        public static readonly DependencyProperty BrushStrokeThicknessProperty =
-            DependencyProperty.Register("BrushStrokeThickness", typeof(double), typeof(CircleProgressBar), new PropertyMetadata(1.0));
 
         private void CircleProgressBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
